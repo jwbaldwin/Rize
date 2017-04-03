d50c0ef979ffb3195dceece4bf2e03c42e8d4cb5
James Baldwin <jbaldwin@infinity-book.local>
2017-04-03 15:40:04 -0400

    Switched from TableView to CollectionView - Added example coupons to view.

---
+++ Rize/Controllers/RZWalletCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  RZWalletCollectionViewCell.swift
+//  Rize
+//
+//  Created by James Baldwin on 4/3/17.
+//  Copyright © 2017 Rize. All rights reserved.
+//
+
+import UIKit
+
+class RZWalletCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var expDate: UILabel!
+    @IBOutlet weak var companyLoc: UILabel!
+    @IBOutlet weak var redeemBtn: UIButton!
+    @IBOutlet weak var infoBtn: UIButton!
+    @IBOutlet weak var companyName: UILabel!
+    @IBOutlet weak var companyLogo: UIImageView!
+}
