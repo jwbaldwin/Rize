629f39b50350b52ba9c43ca332c99ba8dfeeab88
James Baldwin <jbaldwin@Infinity-Book.fios-router.home>
2017-08-22 16:24:47 -0400

    Pod conflicts

--- Pods/Target Support Files/Pods-Rize/Pods-Rize-frameworks.sh
+++ Pods/Target Support Files/Pods-Rize/Pods-Rize-frameworks.sh
@@ -85,7 +85,9 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/AMPopTip/AMPopTip.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/SCLAlertView/SCLAlertView.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/AMPopTip/AMPopTip.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/SCLAlertView/SCLAlertView.framework"
 fi
